dd5d221487bbf004726aaa48409944d4095a218f
Gee4125 <geetha4125@gmail.com>
2024-09-24 15:27:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,25 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {};
 
-// export default nextConfig;
+// // export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       domains: ['img.clerk.com'], // Allow images from this domain
+//     },
+//   };
+  
+//   export default nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['img.clerk.com'], // Allow images from this domain
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['img.clerk.com'], // Allow images from this domain
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during builds
+  },
+};
+
+export default nextConfig;
